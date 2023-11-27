0524a7c0e7283c992c5f9f39162d09cf98e1ee42
rohit <rohitgadekar86@gmail.com>
2023-11-27 18:00:57 +0530

    updated credentials

--- Backend/index.js
+++ Backend/index.js
@@ -2,23 +2,24 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 require('dotenv').config()
-
+//s
 const app = express()
 app.use(express.json())
 app.use(cors())
 
 
 mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true })
+    useNewUrlParser: true
+})
 
 const db = mongoose.connection
-db.on('error', (error)=> console.error(error))
-db.once('open', ()=> console.log('connected to database'))
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('connected to database'))
 
 const usersRouter = require('./routes/user.js')
 app.use('/users', usersRouter)
 
 
-app.listen(8081, ()=>{
+app.listen(8081, () => {
     console.log('started')
 })
